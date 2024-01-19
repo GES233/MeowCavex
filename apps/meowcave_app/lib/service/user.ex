157b46cee1bb766de50e77f4aeb36e991918ed3f
RoastChestnut <chestnut.roasted@outlook.com>
2024-01-19 17:38:08 +0800

    ✨ feat: 引入相关服务

---
+++ apps/meowcave_app/lib/service/user.ex
@@ -0,0 +1,23 @@
+defmodule Service.User do
+  @moduledoc """
+  关于用户的服务。
+  """
+end
+
+defmodule Service.User.Register do
+  @moduledoc """
+  注册服务。
+  """
+end
+
+defmodule Service.User.ModifyInfo do
+  @moduledoc """
+  修改用户的信息。
+  """
+end
+
+defmodule Service.User.UpdateStatus do
+  @moduledoc """
+  更改用户的状态，因为其和信息不同的性质。
+  """
+end
