c8c456a558a37437bcfd71faaf71847e6bd7e7b9
RoastChestnut <chestnut.roasted@outlook.com>
2024-01-19 18:07:40 +0800

    📝 docs: 添加了对配置的翻译（部分）

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,9 @@
 import Config
 
-# Configure your database
+# 配置你的数据库
 #
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
+# MIX_TEST_PARTITION 环境变量可用于在 CI 环境中提供内置测试分区。
+# 运行 `mix help test` 以获得更多信息。
 config :meowcave, MeowCave.Repo,
   username: "postgres",
   password: "postgres",
@@ -13,21 +12,21 @@ config :meowcave, MeowCave.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
+# 我们不在测试时启动服务器。
+# 如果你需要的话，你可以启用以下的服务器设置。
 config :meowcave_web, MeowCaveWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
   secret_key_base: "392ykBPPPbyCxYKJsZ6J4NS6abGWqLfsomat2AmM9bd0hY7T5x3mX3d8MNWNYkYl",
   server: false
 
-# Print only warnings and errors during test
+# 只在测试环境下打印警告以及错误
 config :logger, level: :warning
 
-# In test we don't send emails.
+# 我们在测试环境不发送邮件。
 config :meowcave, MeowCave.Mailer, adapter: Swoosh.Adapters.Test
 
-# Disable swoosh api client as it is only required for production adapters.
+# 禁用 Swoosh API 客户端，因为只有生产适配器才需要它。
 config :swoosh, :api_client, false
 
-# Initialize plugs at runtime for faster test compilation
+# 为加快测试环境下的编译，在运行时初始化 plug
 config :phoenix, :plug_init_mode, :runtime
