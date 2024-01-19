5c75c7d87f1f5686a2629e69a8ec510a7bfbdcc6
RoastChestnut <chestnut.roasted@outlook.com>
2024-01-19 21:36:42 +0800

    ✨ feat: 添加了注册相关的服务

--- apps/meowcave_app/lib/service/user.ex
+++ apps/meowcave_app/lib/service/user.ex
@@ -8,4 +8,37 @@ defmodule Service.User.Register do
   @moduledoc """
   注册服务。
   """
+
+  alias Domain.User
+  alias Domain.User.{Gender, Status}
+  alias Domain.User.Authentication, as: UserAuth
+
+  @spec create_auth(maybe_improper_list() | map()) :: Domain.User.Authentication.t()
+  def create_auth(fields), do: create_auth(fields[:nickname], fields[:email], fields[:password])
+
+  @spec create_auth(charlist() | nil, String.t(), String.t()) ::
+          Domain.User.Authentication.t()
+  def create_auth(nickname, email, password) do
+    %UserAuth{id: nil, nickname: nickname, email: email, password: password}
+  end
+
+  @spec create_blank_user(Domain.User.Authentication.t()) :: Domain.User.t()
+  def create_blank_user(%UserAuth{nickname: nickname} = _userauth, timezone \\ "Etc/UTC") do
+    # 时区信息应该来自 DTO 。
+
+    current = DateTime.utc_now()
+
+    %User{
+      id: nil,
+      username: nil,
+      nickname: nickname,
+      gender: Gender.create(),
+      status: Status.create(),
+      info: "",
+      timezone: timezone,
+      join_at: current
+    }
+  end
+
+  # TODO: Conn with repo.
 end
